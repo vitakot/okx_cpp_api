08168aa6e118063f4870904518241cdfc53f18ac
Vítězslav Kot <v.kot@era.aero>
2025-07-13 17:38:17 +0200

    Fix win build

--- src/okx_ws_stream_manager.cpp
+++ src/okx_ws_stream_manager.cpp
@@ -13,7 +13,12 @@ Copyright (c) 2025 Vitezslav Kot <vitezslav.kot@gmail.com>.
 #include <mutex>
 #include <thread>
 #include <fmt/format.h>
-#include <magic_enum.hpp>
+
+#ifdef _WIN32
+#include "magic_enum/magic_enum.hpp"
+#else
+#include "magic_enum.hpp"
+#endif
 
 using namespace std::chrono_literals;
 
